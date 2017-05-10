8ce4b80301d36580e06cbfda016fe58e24820f83
shadowcatt99 <shadowcatt99@gmail.com>
2017-05-10 11:10:11 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,98 @@
 <!DOCTYPE html>
+
 <html>
 <head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
+  <title>PC1 - Albin</title>
+ 
+ <body>
+ <div name="top" align="center">
+ 	<img src="images/header.png" width="1000" />
+ </div>
+ <div name="middle" align="center">
+ 
+ <table>
+ <td>
+ 	<tr> For data scientists</tr>
+ 	<tr>
+	  	<?php
+		$server="us-cdbr-iron-east-03.cleardb.net";
+		$user="b74ba3320e82ec";
+		$pass="2d194843";
+		$db="ad_bb6ddb2f34daf52";
+		$conn=new mysqli($server, $user, $pass, $db);
+		if($conn->connect_error){
+			die("Connection Failed: ".$conn->connect_error);
+		}
+		$sql="select * from ad_bb6ddb2f34daf52.servicios where codigo_servicio<4";
+		$result=$conn->query($sql);
+		if($result->num_rows > 0){
+			?>
+							
+			<?php
+			while($row=$result->fetch_assoc()){
+				?>
+					
+				<tr>
+					<!--<td><?php echo $row['codigo_servicio']?></td>-->
+					<td><img src="<?php echo $row['imagen_servicio']?>"/></td>
+					<td><?php echo $row['nombre_servicio']?> <br> <?php echo $row['descripcion_servicio']?></td>
+					
+				</tr>
+				
+				<?php
+			}
+		}
+		else{
+			echo "0 result";
+		}
+		$conn->close();
+		?>
+</td>
+<td>
+	<tr>For IT professionals</tr>
+	<tr>
+	  	<?php
+		$server="us-cdbr-iron-east-03.cleardb.net";
+		$user="b74ba3320e82ec";
+		$pass="2d194843";
+		$db="ad_bb6ddb2f34daf52";
+		$conn=new mysqli($server, $user, $pass, $db);
+		if($conn->connect_error){
+			die("Connection Failed: ".$conn->connect_error);
+		}
+		$sql="select * from ad_bb6ddb2f34daf52.servicios where codigo_servicio>3";
+		$result=$conn->query($sql);
+		if($result->num_rows > 0){
+			?>
+							
+			<?php
+			while($row=$result->fetch_assoc()){
+				?>
+					
+				<tr>
+					<!--<td><?php echo $row['codigo_servicio']?></td>-->
+					<td><img src="<?php echo $row['imagen_servicio']?>"/></td>
+					<td><?php echo $row['nombre_servicio']?> <br> <?php echo $row['descripcion_servicio']?></td>
+					
+				</tr>
+				
+				<?php
+			}
+		}
+		else{
+			echo "0 result";
+		}
+		$conn->close();
+		?>
+</td>
+
 	</table>
-</body>
+	 
+	 
+</div>
+ <div name="bottom" align="center">
+ 	<img src="images/bottom.png" width="1000"/>
+ </div>
+  
+ </body>
 </html>
